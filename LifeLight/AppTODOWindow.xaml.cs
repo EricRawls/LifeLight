45f2b1991a74bc3b07529310cce333cffd62a68b
Eric Rawls <EricRawls@users.noreply.github.com>
2025-02-28 06:18:56 -0600

    improved AppTODOWindow

--- LifeLight/AppTODOWindow.xaml.cs
+++ LifeLight/AppTODOWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -22,23 +23,39 @@ namespace LifeLight
     /// </summary>
     public partial class AppTODOWindow : Window
     {
+        private string _strAppToDo = "";
+
+        public string StrAppToDo
+        {
+            get { return _strAppToDo; }
+            set
+            {
+                _strAppToDo = value;
+                OnPropertyChanged(nameof(StrAppToDo));
+            }
+        }
+
         public AppTODOWindow()
         {
             InitializeComponent();
+            DataContext = this; // Set the DataContext to this window
 
-            tbAppTODO.Text += "Build out edit menu.";
-            tbAppTODO.Text += "\n\nAdd ListView for optional items like bath. Add Frequency property in backend (in Add / Edit window). ";
-            tbAppTODO.Text += "\n\nAdd Edit context menu item to change an entry";
+            StrAppToDo += "Build out edit menu.";
+            StrAppToDo += "\n\nAdd ListView for optional daily items. Add Frequency property in backend (and in Add / Edit window).";
+            StrAppToDo += "\nItems will show label indicating previous instance and next due based on frequency.";
+            StrAppToDo += "\n\nAdd Edit context menu item to change an entry";
+            StrAppToDo += "\n\nAdd mood rating trackbar";
+            StrAppToDo += "\n\nAdd daily comments textbox";
+            StrAppToDo += "\n\nAdd Reports button to show reports window with checkboxes for each item to report, ";
+            StrAppToDo += "start date calendar, end date calendar, report button on that window.";
+        }
 
-            tbAppTODO.Text += "\n\nAdd mood rating trackbar";
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged = null!;
 
-            tbAppTODO.Text += "\n\nAdd daily comments textbox";
-
-            tbAppTODO.Text += "\n\nAdd Reports button to show reports window with checkboxes for each item to report, ";
-            tbAppTODO.Text += "start date calendar, end date calendar, report button on that window.";
-
-
-
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         private void OK_Click(object sender, RoutedEventArgs e)
         {
